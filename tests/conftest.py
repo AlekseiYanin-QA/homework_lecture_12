0c79b4f2ea677b241e342009ced3651d5bddc7bf
Yanin.A.V <aleksei.yanin.qa@gmail.com>
2024-02-18 12:32:25 +0300

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,13 +25,14 @@ def setup_browser(request):
     browser = Browser(Config(driver))
     yield browser
 
-@pytest.fixture(scope='function', autouse=True)
-def browser_management():
-    browser.config.base_url = 'https://demoqa.com/'
-    browser.config.driver = 'chrome'
-    browser.config.window_height = 900
-    browser.config.window_width = 1200
 
-    yield
-
-    browser.quit()
+# @pytest.fixture(scope='function', autouse=True)
+# def browser_management():
+#     browser.config.base_url = 'https://demoqa.com/'
+#     browser.config.driver = 'chrome'
+#     browser.config.window_height = 900
+#     browser.config.window_width = 1200
+#
+#     yield
+#
+#     browser.quit()
